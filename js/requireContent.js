68e6508a928c29deafef2f18efdf0f70a9c78a79
Martin Havelka <martin@salsitasoft.com>
2012-10-23 09:41:36 +0200

    new version of requireContent.js

--- js/requireContent.js
+++ js/requireContent.js
@@ -1,12 +1,9 @@
 require.load = function (context, moduleName, url) {
   var xhr;
-  var complete = false;
-  onScriptLoad = context.onScriptLoad;
   xhr = new XMLHttpRequest();
   xhr.open("GET", chrome.extension.getURL(url) + '?r=' + new Date().getTime(), true);
   xhr.onreadystatechange = function (e) {
-    if (xhr.readyState === 4 && !complete) {
-      complete = true;
+    if (xhr.readyState === 4 && xhr.status === 200) {
       eval(xhr.responseText);
       context.completeLoad(moduleName)
     }
