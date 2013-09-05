95a92243b9770284823fbcc587165dbb4c2fdfbf
Daniel Prentis <dprentis@seznam.cz>
2013-09-05 18:04:10 +0200

    Eval scripts with correct 'this' setting

--- code/js/requireContent.js
+++ code/js/requireContent.js
@@ -1,12 +1,15 @@
-require.load = function (context, moduleName, url) {
-  var xhr;
-  xhr = new XMLHttpRequest();
-  xhr.open("GET", chrome.extension.getURL(url) + '?r=' + new Date().getTime(), true);
-  xhr.onreadystatechange = function (e) {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-      eval(xhr.responseText);
-      context.completeLoad(moduleName)
-    }
+(function() {
+  var global = this;
+  require.load = function (context, moduleName, url) {
+    var xhr;
+    xhr = new XMLHttpRequest();
+    xhr.open("GET", chrome.extension.getURL(url) + '?r=' + new Date().getTime(), true);
+    xhr.onreadystatechange = function (e) {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        eval.call(global, xhr.responseText);
+        context.completeLoad(moduleName)
+      }
+    };
+    xhr.send(null);
   };
-  xhr.send(null);
-};
+})();
