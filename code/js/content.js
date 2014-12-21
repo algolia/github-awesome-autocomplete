78000bf77edbb4b6833da45929132fa06266a92f
Shipow <shipowlata@gmail.com>
2014-12-21 02:44:48 +0100

    fix highlight

--- code/js/content.js
+++ code/js/content.js
@@ -20,7 +20,7 @@
   var templateYourRepo = Hogan.compile('<div class="aa-suggestion aa-your-repo">' +
     '<span class="aa-name">' +
       '<i class="octicon {{#fork}}octicon-repo-forked{{/fork}}{{^fork}}{{#private}}octicon-lock{{/private}}{{^private}}octicon-repo{{/private}}{{/fork}}"></i> ' +
-      '<a href="https://github.com/{{ full_name }}/"><span class="owner">{{{ owner }}}</span>/<span class="repo">{{{ highlightedName }}}</span></a>' +
+      '<a href="https://github.com/{{ full_name }}/"><span class="owner">{{{ owner }}}</span>/<span class="aa-repo">{{{ highlightedName }}}</span></a>' +
     '</span>' +
   '</div>');
 
