80bc82ceb4208e92e92cc237c8518c9ddbf2fbe2
Sylvain UTARD <sylvain.utard@gmail.com>
2014-12-21 01:14:21 +0100

    Handle private suggestion selection as well

--- code/js/content.js
+++ code/js/content.js
@@ -188,7 +188,7 @@
     ]).on('typeahead:selected', function(event, suggestion, dataset) {
       if (dataset === 'users') {
         location.href = 'https://github.com/' + suggestion.login;
-      } else if (dataset === 'repos') {
+      } else if (dataset === 'repos' || dataset === 'private') {
         location.href = 'https://github.com/' + suggestion.full_name;
       } else {
         console.log('Unknown dataset: ' + dataset);
