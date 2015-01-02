7105bcfc939a5b856b567f6507fc3b8497d6f183
Sylvain UTARD <sylvain.utard@gmail.com>
2015-01-02 13:56:00 +0100

    Use non-browserified code here

--- Gruntfile.js
+++ Gruntfile.js
@@ -77,7 +77,7 @@ module.exports = function(grunt) {
           'cp code/js/firefox/main.js build/firefox/index.js',
           'rm -rf build/firefox/data', 'mkdir build/firefox/data',
           'cp -R code/js/libs build/firefox/data',
-          'cp build/unpacked-dev/js/content.js build/firefox/data/content.js',
+          'cp code/js/content.js build/firefox/data/content.js',
           'cp code/images/* build/firefox/data/',
           'cp code/html/* build/firefox/data/',
           'cp code/css/* build/firefox/data/',
