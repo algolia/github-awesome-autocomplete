a71ad3e56a8dc7161cf0397673536040dcabe9a7
Sylvain UTARD <sylvain.utard@gmail.com>
2016-02-20 00:16:56 +0100

    Fixed missing icons
    All icons moved from CSS to SVG.
    
    Fix #29

--- code/js/content.js
+++ code/js/content.js
@@ -17,29 +17,36 @@ var NB_ISSUES = 3;
 //
 /////////////////////////////
 
+var octiconStar = '<svg aria-hidden="true" class="octicon octicon-star" height="16" role="img" version="1.1" viewBox="0 0 14 16" width="14"><path d="M14 6l-4.9-0.64L7 1 4.9 5.36 0 6l3.6 3.26L2.67 14l4.33-2.33 4.33 2.33L10.4 9.26 14 6z"></path></svg>';
+var octiconFork = '<svg aria-hidden="true" class="octicon octicon-git-branch" height="16" role="img" version="1.1" viewBox="0 0 10 16" width="10"><path d="M10 5c0-1.11-0.89-2-2-2s-2 0.89-2 2c0 0.73 0.41 1.38 1 1.72v0.3c-0.02 0.52-0.23 0.98-0.63 1.38s-0.86 0.61-1.38 0.63c-0.83 0.02-1.48 0.16-2 0.45V4.72c0.59-0.34 1-0.98 1-1.72 0-1.11-0.89-2-2-2S0 1.89 0 3c0 0.73 0.41 1.38 1 1.72v6.56C0.41 11.63 0 12.27 0 13c0 1.11 0.89 2 2 2s2-0.89 2-2c0-0.53-0.2-1-0.53-1.36 0.09-0.06 0.48-0.41 0.59-0.47 0.25-0.11 0.56-0.17 0.94-0.17 1.05-0.05 1.95-0.45 2.75-1.25s1.2-1.98 1.25-3.02h-0.02c0.61-0.36 1.02-1 1.02-1.73zM2 1.8c0.66 0 1.2 0.55 1.2 1.2s-0.55 1.2-1.2 1.2-1.2-0.55-1.2-1.2 0.55-1.2 1.2-1.2z m0 12.41c-0.66 0-1.2-0.55-1.2-1.2s0.55-1.2 1.2-1.2 1.2 0.55 1.2 1.2-0.55 1.2-1.2 1.2z m6-8c-0.66 0-1.2-0.55-1.2-1.2s0.55-1.2 1.2-1.2 1.2 0.55 1.2 1.2-0.55 1.2-1.2 1.2z"></path></svg>';
+var octiconRepo = '<svg aria-hidden="true" class="octicon octicon-repo repo-icon" height="16" role="img" version="1.1" viewBox="0 0 12 16" width="12"><path d="M4 9h-1v-1h1v1z m0-3h-1v1h1v-1z m0-2h-1v1h1v-1z m0-2h-1v1h1v-1z m8-1v12c0 0.55-0.45 1-1 1H6v2l-1.5-1.5-1.5 1.5V14H1c-0.55 0-1-0.45-1-1V1C0 0.45 0.45 0 1 0h10c0.55 0 1 0.45 1 1z m-1 10H1v2h2v-1h3v1h5V11z m0-10H2v9h9V1z"></path></svg>';
+var octiconLock = '<svg aria-hidden="true" class="octicon octicon-lock repo-icon" height="16" role="img" version="1.1" viewBox="0 0 12 16" width="12"><path d="M4 13h-1v-1h1v1z m8-6v7c0 0.55-0.45 1-1 1H1c-0.55 0-1-0.45-1-1V7c0-0.55 0.45-1 1-1h1V4C2 1.8 3.8 0 6 0s4 1.8 4 4v2h1c0.55 0 1 0.45 1 1z m-8.2-1h4.41V4c0-1.22-0.98-2.2-2.2-2.2s-2.2 0.98-2.2 2.2v2z m7.2 1H2v7h9V7z m-7 1h-1v1h1v-1z m0 2h-1v1h1v-1z"></path></svg>';
+var octiconComment = '<svg aria-hidden="true" class="octicon octicon-comment-discussion" height="16" role="img" version="1.1" viewBox="0 0 16 16" width="16"><path d="M15 2H6c-0.55 0-1 0.45-1 1v2H1c-0.55 0-1 0.45-1 1v6c0 0.55 0.45 1 1 1h1v3l3-3h4c0.55 0 1-0.45 1-1V10h1l3 3V10h1c0.55 0 1-0.45 1-1V3c0-0.55-0.45-1-1-1zM9 12H4.5l-1.5 1.5v-1.5H1V6h4v3c0 0.55 0.45 1 1 1h3v2z m6-3H13v1.5l-1.5-1.5H6V3h9v6z"></path></svg>';
+var octiconOrganization = '<svg aria-hidden="true" class="octicon octicon-organization" height="16" role="img" version="1.1" viewBox="0 0 14 16" width="14"><path d="M4.75 4.95c0.55 0.64 1.34 1.05 2.25 1.05s1.7-0.41 2.25-1.05c0.34 0.63 1 1.05 1.75 1.05 1.11 0 2-0.89 2-2s-0.89-2-2-2c-0.41 0-0.77 0.13-1.08 0.33C9.61 1 8.42 0 7 0S4.39 1 4.08 2.33c-0.31-0.2-0.67-0.33-1.08-0.33-1.11 0-2 0.89-2 2s0.89 2 2 2c0.75 0 1.41-0.42 1.75-1.05z m5.2-1.52c0.2-0.38 0.59-0.64 1.05-0.64 0.66 0 1.2 0.55 1.2 1.2s-0.55 1.2-1.2 1.2-1.17-0.53-1.19-1.17c0.06-0.19 0.11-0.39 0.14-0.59zM7 0.98c1.11 0 2.02 0.91 2.02 2.02s-0.91 2.02-2.02 2.02-2.02-0.91-2.02-2.02S5.89 0.98 7 0.98zM3 5.2c-0.66 0-1.2-0.55-1.2-1.2s0.55-1.2 1.2-1.2c0.45 0 0.84 0.27 1.05 0.64 0.03 0.2 0.08 0.41 0.14 0.59-0.02 0.64-0.53 1.17-1.19 1.17z m10 0.8H1c-0.55 0-1 0.45-1 1v3c0 0.55 0.45 1 1 1v2c0 0.55 0.45 1 1 1h1c0.55 0 1-0.45 1-1v-1h1v3c0 0.55 0.45 1 1 1h2c0.55 0 1-0.45 1-1V12h1v1c0 0.55 0.45 1 1 1h1c0.55 0 1-0.45 1-1V11c0.55 0 1-0.45 1-1V7c0-0.55-0.45-1-1-1zM3 13h-1V10H1V7h2v6z m7-2h-1V9h-1v6H6V9h-1v2h-1V7h6v4z m3-1h-1v3h-1V7h2v3z"></path></svg>';
+
 var templateYourRepo = Hogan.compile('<div class="aa-suggestion aa-your-repo">' +
   '<span class="aa-name">' +
-    '<i class="octicon {{#fork}}octicon-repo-forked{{/fork}}{{^fork}}{{#private}}octicon-lock{{/private}}{{^private}}octicon-repo{{/private}}{{/fork}}"></i> ' +
+    '{{#fork}}' + octiconFork + '{{/fork}}{{^fork}}{{#private}}' + octiconLock + '{{/private}}{{^private}}' + octiconRepo + '{{/private}}{{/fork}} ' +
     '<a href="https://github.com/{{ full_name }}/"><span class="owner">{{{ owner }}}</span>/<span class="aa-repo">{{{ highlightedName }}}</span></a>' +
   '</span>' +
 '</div>');
 var templateRepo = Hogan.compile('<div class="aa-suggestion aa-repo">' +
   '<div class="aa-thumbnail"><img src="https://avatars.githubusercontent.com/{{ owner }}?size=30" /></div>' +
-  '<div class="aa-infos">{{ watchers }} <i class="octicon octicon-star"></i></div>' +
+  '<div class="aa-infos">' + octiconStar + ' {{ watchers }}</div>' +
   '<span class="aa-name">' +
-    '{{#is_fork}}<i class="octicon octicon-repo-forked"></i>{{/is_fork}} ' +
-    '{{^is_fork}}<i class="octicon octicon-{{#is_private}}lock{{/is_private}}{{^is_private}}repo{{/is_private}}"></i>{{/is_fork}} ' +
+    '{{#is_fork}}' + octiconFork + '{{/is_fork}} ' +
+    '{{^is_fork}}{{#is_private}}' + octiconLock + '{{/is_private}}{{^is_private}}' + octiconRepo + '{{/is_private}}{{/is_fork}} ' +
     '<a href="https://github.com/{{ full_name }}/"><span class="aa-owner">{{{ owner }}}</span>/<span class="aa-repo-name">{{{ _highlightResult.name.value }}}</span></a>' +
   '</span>' +
   '<div class="aa-description">{{{ _snippetResult.description.value }}}</div>' +
 '</div>');
 var templateIssue = Hogan.compile('<div class="aa-suggestion aa-issue">' +
   '<div class="aa-thumbnail"><img src="https://avatars.githubusercontent.com/{{ repository.owner }}?size=30" /></div>' +
-  '<div class="aa-infos"><i class="octicon octicon-comment"></i> {{ comments_count }}</div>' +
+  '<div class="aa-infos">' + octiconComment + ' {{ comments_count }}</div>' +
   '<span class="aa-name">' +
     '<span class="aa-issue-number">Issue #{{ number }}:</span> <a href="https://github.com/{{ repository.owner }}/{{ repository.name }}/issues/{{ number }}">{{{ _highlightResult.title.value }}}</a><br />' +
-    '{{#repository.is_fork}}<i class="octicon octicon-repo-forked"></i>{{/repository.is_fork}} ' +
-    '{{^repository.is_fork}}<i class="octicon octicon-{{#repository.is_private}}lock{{/repository.is_private}}{{^repository.is_private}}repo{{/repository.is_private}}"></i>{{/repository.is_fork}} ' +
+    '{{#repository.is_fork}}' + octiconFork + '{{/repository.is_fork}} ' +
+    '{{^repository.is_fork}}{{#repository.is_private}}' + octiconLock + '{{/repository.is_private}}{{^repository.is_private}}' + octiconRepo + '{{/repository.is_private}}{{/repository.is_fork}} ' +
     '{{ repository.owner }}/<span class="aa-repo-name">{{{ _highlightResult.repository.name.value }}}</span>' +
   '</span>' +
   '<div class="aa-issue-body">{{{ _snippetResult.body.value }}}</div>' +
@@ -50,7 +57,7 @@ var templateUser = Hogan.compile('<div class="aa-suggestion aa-user">' +
     '{{#name}}<span class="aa-name">{{{ _highlightResult.name.value }}}</span> {{/name}}' +
     '<span class="aa-login">{{{ _highlightResult.login.value }}}</span>' +
   '</a>' +
-  '{{#company}}<br><span class="aa-company"><i class="octicon octicon-organization"></i> {{{ _highlightResult.company.value }}}</span>{{/company}}' +
+  '{{#company}}<br><span class="aa-company">' + octiconOrganization + ' {{{ _highlightResult.company.value }}}</span>{{/company}}' +
 '</div>');
 
 /////////////////////////////
@@ -335,7 +342,7 @@ $(document).ready(function() {
       displayKey: 'query',
       templates: {
         suggestion: function(hit) {
-          return '<div class="aa-query"><i class="octicon octicon-repo"></i>&nbsp; Search "' + sanitize(hit.query) + '" in this repository</div>';
+          return '<div class="aa-query">' + octiconRepo + '&nbsp; Search "' + sanitize(hit.query) + '" in this repository</div>';
         }
       }
     },
@@ -495,7 +502,7 @@ $(document).ready(function() {
       templates: {
         empty: function() {
           return '<div class="aa-branding">' +
-            'With <i class="octicon octicon-heart"></i> from <a href="https://www.algolia.com/?utm_source=github-awesome-autocomplete&utm_medium=link&utm_campaign=github-awesome-autocomplete_search"><img src="' + getURL('images/algolia128x40.png') + '" title="Algolia" /></a>' +
+            'With <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 510 510"><path d="M255 489.6l-35.7-35.7C86.7 336.6 0 257.55 0 160.65 0 81.6 61.2 20.4 140.25 20.4c43.35 0 86.7 20.4 114.75 53.55C283.05 40.8 326.4 20.4 369.75 20.4 448.8 20.4 510 81.6 510 160.65c0 96.9-86.7 175.95-219.3 293.25L255 489.6z" fill="#D80027"/></svg> from <a href="https://www.algolia.com/?utm_source=github-awesome-autocomplete&utm_medium=link&utm_campaign=github-awesome-autocomplete_search"><img src="' + getURL('images/algolia128x40.png') + '" title="Algolia" /></a>' +
             '</div>';
         }
       }
